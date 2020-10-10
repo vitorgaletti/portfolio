3a7ac4e5ec06818ab472ed3eb36d4d18aa6995af
Vitor Galeti <vitor.fgaletti@gmail.com>
2020-10-10 13:38:40 -0300

    preloader-fazendo o javascript da tela de preloader

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,5 +1,20 @@
+//Declarando Variáveis
 var btnContact = document.querySelector('.vg-btn-contact');
 
+
+//Page Preloader
+window.addEventListener('load', function () {
+    var pagePreloader = document.querySelector('.vg-preloader');
+    pagePreloader.classList.add('vg-fade-out');
+
+    setTimeout(function () {
+        pagePreloader.style.display = 'none';
+    }, 2000);
+
+
+});
+
+//Abrindo e Fechando Informações de Contato
 btnContact.addEventListener('click', function(){
     var boxContact = document.querySelector('.vg-contact-info');
     boxContact.classList.toggle('vg-is-open');

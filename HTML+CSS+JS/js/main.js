a0a608763fef18cfe9820410fe3a922155fd4d4f
Vitor Galeti <vitor.fgaletti@gmail.com>
2020-11-07 14:48:47 -0300

    responsividade-menu mobile

--- HTML+CSS+JS/js/main.js
+++ HTML+CSS+JS/js/main.js
@@ -1,6 +1,9 @@
 //Declarando Variáveis
 var btnContact = document.querySelector('.vg-btn-contact');
-
+var toggleMenu = document.querySelectorAll('.vg-toggle-menu');
+var menuMobile = document.querySelector('.vg-menu-mob');
+var overlay = document.querySelector('.vg-menu-overlay');
+var btnMenuMobIcon = document.querySelector('.vg-btn-menu-mob ion-icon');
 
 //Page Preloader
 window.addEventListener('load', function () {
@@ -11,8 +14,8 @@ window.addEventListener('load', function () {
         pagePreloader.style.display = 'none';
     }, 2000);
 
-
 });
+
 
 //Abrindo e Fechando Informações de Contato
 btnContact.addEventListener('click', function () {
@@ -20,6 +23,25 @@ btnContact.addEventListener('click', function () {
     boxContact.classList.toggle('vg-is-open');
     this.classList.toggle('vg-change-icon');
 });
+
+//Abrindo e Fechando o Menu Mobile
+for (var m = 0; m < toggleMenu.length; m++) {
+    toggleMenu[m].addEventListener('click', function () {
+        overlay.classList.toggle('vg-is-open');
+        menuMobile.classList.toggle('vg-menu-is-closed');
+        menuMobile.classList.toggle('vg-menu-is-open');
+
+        var icon = btnMenuMobIcon.getAttribute('name');
+
+        if (icon === 'menu-outline') {
+            btnMenuMobIcon.setAttribute('name', 'close-outline');
+        } else {
+            btnMenuMobIcon.setAttribute('name', 'menu-outline');
+        }
+    });
+
+}
+
 
 var postGallery = document.querySelector('.vg-post-gallery');
 var postGalleryHeight = postGallery.clientHeight;
